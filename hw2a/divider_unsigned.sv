acb389c20a265d98104c72cef37258b9543b3dfe
manask-02 <manask@seas.upenn.edu>
2024-03-15 19:56:40 -0400

    haha

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -4,18 +4,6 @@
 
 // quotient = dividend / divisor
 
-module divider_unsigned (
-    input  wire [31:0] i_dividend,
-    input  wire [31:0] i_divisor,
-    output wire [31:0] o_remainder,
-    output wire [31:0] o_quotient
-);
-
-    // TODO: your code here
-
-endmodule
-
-
 module divu_1iter (
     input  wire [31:0] i_dividend,
     input  wire [31:0] i_divisor,
@@ -25,19 +13,52 @@ module divu_1iter (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-  /*
-    for (int i = 0; i < 32; i++) {
-        remainder = (remainder << 1) | ((dividend >> 31) & 0x1);
-        if (remainder < divisor) {
-            quotient = (quotient << 1);
-        } else {
-            quotient = (quotient << 1) | 0x1;
-            remainder = remainder - divisor;
-        }
-        dividend = dividend << 1;
-    }
-    */
+    wire [31:0] m_remainder;
+    wire [31:0] m_remainder1;
+    wire [31:0] m_remainder3;
+    wire [31:0] m_remainder2;
+    wire x;
 
-    // TODO: your code here
+    assign m_remainder = {i_remainder[30:0], 1'b0};
+    assign m_remainder1 = {31'b0, i_dividend[31]};
+    assign m_remainder2 = m_remainder1 & {31'b0, 1'b1};
+    assign m_remainder3 = m_remainder | m_remainder2;
+
+    assign x = (m_remainder3 < i_divisor);
+
+    assign o_quotient = (x) ? {i_quotient[30:0], 1'b0} : {i_quotient[30:0], 1'b1}; 
+    assign o_remainder = (x) ? m_remainder3 : (m_remainder3 - i_divisor);
+    assign o_dividend = {i_dividend[30:0], 1'b0};
+
+endmodule
+
+module divider_unsigned (
+    input  wire [31:0] i_dividend,
+    input  wire [31:0] i_divisor,
+    output wire [31:0] o_remainder,
+    output wire [31:0] o_quotient
+);
+
+    wire [31:0] remainder [32:0];
+    wire [31:0] quotient [32:0] ;
+    wire [31:0] dividend [32:0];   
+    
+    genvar i;
+always_comb begin
+        for (i = 0; i < 32; i = i + 1) begin 
+            divu_1iter d (
+                .i_dividend(dividend[i]),
+                .i_divisor(i_divisor),
+                .i_remainder(remainder[i]),
+                .i_quotient(quotient[i]),
+                .o_dividend(dividend[i+1]),
+                .o_remainder(remainder[i+1]),
+                .o_quotient(quotient[i+1])
+            );
+        end
+   end
+
+    assign o_remainder = remainder[32];
+    assign o_quotient = quotient[32];
 
 endmodule
